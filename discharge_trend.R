0bcc1338108aeca0c43b10e0340609a87d0d709b
David Kahler <dmkahler@users.noreply.github.com>
2023-03-26 22:29:43 -0400

    Add files via upload

---
+++ discharge_trend.R
@@ -0,0 +1,142 @@
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+# SEA-LEVEL RISE
+s <- read_csv("/Users/davidkahler/Documents/Hydrology_and_WRM/Limpopo_Basin_Study/Land_Movement_Gabi/d181_Durban_Daily_RawData_UHSLC.csv")
+t <- s %>%
+     group_by(Hydrologic_Year) %>%
+     summarize(mn=mean(Value, na.rm = TRUE)/1000) # initially in mm
+
+n <- lm(t$mn~t$Hydrologic_Year)
+summary(n)
+confint(n)
+# Estimate Std. Error t value Pr(>|t|)  
+# (Intercept)       -1.0813041  0.8430176  -1.283    0.206  
+# t$Hydrologic_Year  0.0011073  0.0004226   2.620    0.012 *
+#      ---
+#      Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 
+#                          2.5 %      97.5 %
+# (Intercept)       -2.7802944621 0.617686331
+# t$Hydrologic_Year  0.0002555817 0.001959008
+
+ggplot(t) +
+     geom_point(aes(x=Hydrologic_Year, y=mn)) +
+     geom_smooth(aes(x=Hydrologic_Year,y=mn), method = "lm", se = TRUE, color='blue') +
+     xlab("Hydrologic Year") +
+     ylab("Mean Sea-Level Height (m)") +
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 14)) +
+     theme(axis.title = element_text(face = "plain", size = 14))
+
+######################### Xai Xai #############################################
+# Read in CSV of river height data from gauge station E-38 at XaiXai
+# Data provided by ARA-SUL
+data_E38 <- read_csv("/Users/davidkahler/Documents/Hydrology_and_WRM/Limpopo_Basin_Study/Land_Movement_Gabi/Xai_Xai_WaterHeight_StationE38_RAW.csv")
+annual_E38 <- data_E38 %>%
+     rename(hydroYear=`Hydrologic Year`) %>%
+     #drop_na(Mean) %>%
+     group_by(hydroYear) %>%
+     summarize(mn=mean(Mean, na.rm=TRUE)) # original data in m
+
+model_E38 <- lm(annual_E38$mn ~ annual_E38$hydroYear)
+# Coefficients:
+#                        Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)          40.755880   9.701082   4.201 0.000369 ***
+#      annual_E38$hydroYear -0.019414   0.004832  -4.017 0.000578 ***
+#      ---
+#      Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#                                 2.5 %       97.5 %
+#      (Intercept)          20.63706833 60.874692430
+# annual_E38$hydroYear -0.02943563 -0.009392097
+
+E38 <- ggplot(annual_E38) +
+     geom_point(aes(x=hydroYear, y=mn)) +
+     geom_smooth(aes(x=hydroYear,y=mn), method = "lm", se = TRUE, color='blue') +
+     xlab("Hydrologic Year") +
+     ylab("Mean River Height (m)") +
+     annotate("text", x=2016.5, y=2.21, label = "E38", size = 6) +
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(aspect.ratio = 0.5) +
+     theme(axis.text = element_text(face = "plain", size = 14)) +
+     theme(axis.title = element_text(face = "plain", size = 14))
+gE38 <- ggplotGrob(E38)
+
+######################### Chokwe #############################################
+# Read in CSV of river height data from gauge station E-35 at Chokwe
+# Data provided by ARA-SUL
+data_E35 <- read.csv("/Users/davidkahler/Documents/Hydrology_and_WRM/Limpopo_Basin_Study/Land_Movement_Gabi/Chokwe_WaterHeight_StationE35.csv")
+
+annual_E35 <- data_E35 %>%
+     rename(hydroYear=`Hydrologic_Year`) %>%
+     #drop_na(Mean) %>%
+     group_by(hydroYear) %>%
+     summarize(mn=mean(Media, na.rm=TRUE)) # original data in m
+
+model_E35 <- lm(annual_E35$mn ~ annual_E35$hydroYear)
+# Coefficients:
+#      Estimate Std. Error t value Pr(>|t|)
+# (Intercept)          37.66277   53.30639   0.707    0.489
+# annual_E35$hydroYear -0.01811    0.02652  -0.683    0.504
+# > confint(model_E35)
+# 2.5 %       97.5 %
+#      (Intercept)          -74.80388887 150.12943039
+# annual_E35$hydroYear  -0.07406813   0.03783858
+
+E35 <- ggplot(annual_E35) +
+     geom_point(aes(x=hydroYear, y=mn)) +
+     geom_smooth(aes(x=hydroYear,y=mn), method = "lm", se = TRUE, color='blue') +
+     xlab("Hydrologic Year") +
+     ylab("Mean River Height (m)") +
+     annotate("text", x=2017, y=2, label = "E35", size = 6) +
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(aspect.ratio = 0.5) +
+     theme(axis.text = element_text(face = "plain", size = 14)) +
+     theme(axis.title = element_text(face = "plain", size = 14))
+gE35 <- ggplotGrob(E35)
+
+grid::grid.newpage()
+rivers <- grid::grid.draw(rbind(gE38,gE35))
+#ggsave("f02_riverheighs.eps", rivers, device = "eps", dpi = 72)
+
+
+# BEITBRIDGE DISCHARGE
+x <- read_csv("/Users/davidkahler/Documents/Hydrology_and_WRM/Limpopo_Basin_Study/Land_Movement_Gabi/beitbridge_discharge.csv")
+y <- x %>%
+     select(Year,`Height (m)`) %>%
+     rename(height=`Height (m)`)
+
+m <- lm(y$height~y$Year)
+summary(m)
+confint(m)
+# Call:
+#      lm(formula = y$height ~ y$Year)
+# Residuals:
+#      Min       1Q   Median       3Q      Max 
+# -0.23808 -0.16895 -0.01556  0.12205  0.32043 
+# Coefficients:
+#      Estimate Std. Error t value Pr(>|t|)
+# (Intercept)  1.3666340  7.5547394   0.181    0.858
+# y$Year      -0.0004995  0.0037623  -0.133    0.895
+# Residual standard error: 0.1741 on 26 degrees of freedom
+# (3 observations deleted due to missingness)
+# Multiple R-squared:  0.0006776,	Adjusted R-squared:  -0.03776 
+# F-statistic: 0.01763 on 1 and 26 DF,  p-value: 0.8954
+#                   2.5 %       97.5 %
+# (Intercept) -14.16235519 16.895623166
+# y$Year       -0.00823302  0.007233954
+
+ggplot(y) +
+     geom_point(aes(x=Year,y=height)) +
+     geom_smooth(aes(x=Year,y=height), method = "lm", se = TRUE, color='blue') +
+     xlab("Hydrologic Year") +
+     ylab("Mean Gauge Height (m)") +
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 14)) +
+     theme(axis.title = element_text(face = "plain", size = 14))
+
+
+
+
---
+++ dws_gage_vis.R
@@ -0,0 +1,170 @@
+# To visualize data scraped from DWS gages near Musina
+
+library(tidyverse)
+library(latex2exp)
+# install_github("LimpopoLab/hydrostats")
+library(hydrostats)
+
+x <- read_csv("A7H004.csv", col_names = FALSE)
+x <- x %>%
+     rename(dt=X1,UNIX=X2,lev_m=X3,levQC=X4,flow_m3s=X5,flowQC=X6) # note UNIX date time is UTC
+
+y <- read_csv("A7H008.csv", col_names = FALSE)
+y <- y %>%
+     rename(dt=X1,UNIX=X2,lev_m=X3,levQC=X4,flow_m3s=X5,flowQC=X6) # note UNIX date time is UTC
+
+z <- rbind(x,y)
+# Remove duplicate dates
+q <- z %>%
+     mutate(lev_m=replace(lev_m, which(lev_m<=0),NA)) %>%
+     mutate(flow_m3s=replace(flow_m3s, which(flow_m3s<=0),NA)) %>%
+     group_by(UNIX) %>%
+     summarize(lev_m=mean(lev_m,na.rm=TRUE), levQC=mean(levQC), flow_m3s=mean(flow_m3s,na.rm=TRUE), flowQC=mean(flowQC)) %>%
+     mutate(dt=with_tz(as_datetime(UNIX), tzone = "Africa/Johannesburg"))
+
+ggplot(q) +
+     geom_point(aes(x=dt,y=flow_m3s)) +
+     ylim(c(0,10000)) +
+     xlab("Date") + 
+     ylab(TeX('Discharge $(m^3/s)$')) + 
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 12))
+
+## Annual data
+a <- q %>%
+     mutate(hydro_year=hyd.yr(dt, h = "S")) %>%
+     group_by(hydro_year) %>%
+     summarize(ann.min.flow=min(flow_m3s,na.rm=TRUE),ann.mean.flow=mean(flow_m3s,na.rm=TRUE),ann.max.flow=max(flow_m3s,na.rm=TRUE)) %>%
+     mutate(annual.tot=ann.mean.flow*3600*24*365.25) #%>%
+     #na_if(Inf) %>%
+     #na_if(-Inf) %>%
+     #na_if(NaN)
+# it appears that the warnings, length > 1, non-missing arguments, are taken care of.
+
+# Annual flow time series
+# Average annual flow
+ave.ann.flow <- 365.25 * 24 * 3600 * mean(m$mon.mean.flow)
+# Found the annual mean flow to be 1,689,908,081 m^3, that is, 1.7x10^9 m^3
+
+modBeitbridge <- lm(a$annual.tot ~ a$hydro_year)
+# Coefficients:
+#                     Estimate Std. Error t value Pr(>|t|)   
+#      (Intercept)   1.748e+11  5.308e+10   3.293  0.00168 **
+#      a$hydro_year -8.566e+07  2.669e+07  -3.210  0.00215 **
+#      ---
+#      Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#                         2.5 %       97.5 %
+#      (Intercept)  68580428714 280986119165
+#      a$hydro_year  -139060814    -32260553
+
+ggplot(a) + #   ann_flow <- 
+     geom_point(aes(x=hydro_year,y=(annual.tot/1e9))) +
+     #geom_hline(yintercept = ave.ann.flow) +
+     geom_smooth(aes(x=hydro_year,y=(annual.tot/1e9)), method = "lm", se = TRUE, color='blue') +
+     xlim(c(1955,2022)) +
+     ylim(c(0,30)) +
+     #coord_cartesian(ylim=c(0,10000000000)) + # control y lim here!
+     xlab("Hydrologic Year") + 
+     ylab(TeX('Annual Total Discharge $(\\times 10^{9} m^3/y)$')) + 
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(legend.position="right") + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 14)) +
+     theme(axis.title = element_text(face = "plain", size = 14))
+ggsave("annual_flow_tall.eps", ann_flow, device = "eps", dpi = 72)
+
+## Annual flood
+ann_flood <- ggplot(a) +
+     geom_col(aes(x=hydro_year,y=ann.max.flow)) +
+     xlim(c(1955,2022)) +
+     ylim(c(0,10000)) +
+     xlab("Hydrologic Year") + 
+     ylab(TeX('Annual Maximum Discharge $(m^3/s)$')) + 
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(legend.position="right") + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 12))
+ggsave("annual_flood.eps", ann_flood, device = "eps", dpi = 72)
+
+ann_flood_hist <- ggplot(a,aes(x=ann.max.flow)) +
+     geom_histogram(breaks = (500*c(0:20)), color = "black", fill = "gray", na.rm = TRUE) +
+     xlab(TeX('Annual Maximum Discharge $(m^3/s)$')) +
+     ylab("Years") +
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(legend.position="right") + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 12))
+ggsave("annual_flood_hist.eps", ann_flood_hist, device = "eps", dpi = 72)
+
+# 200 year flood
+a$log_q <- log(a$ann.max.flow, base = 10) # log transform
+T_R <- 200 # return period in years (in this case)
+Fx <- 1 - (1/T_R)
+mu <- mean(a$log_q, na.rm = TRUE)
+s <- sd(a$log_q, na.rm = TRUE)
+c <- skew(a$log_q) # NA removed by default
+lp3.x <- pt3(c,Fx)
+lp3.y <- (lp3.x * s) + mu
+lp3.z <- 10^lp3.y # this is the T_R flood level 200-year flood is 13,667 m^3/s
+
+ann_drought <- ggplot(a) +
+     geom_col(aes(x=hydro_year,y=ann.min.flow)) +
+     xlim(c(1955,2022)) +
+     ylim(c(0,10)) +
+     xlab("Hydrologic Year") + 
+     ylab(TeX('Annual Minimum Discharge $(m^3/s)$')) + 
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(legend.position="right") + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 12))
+ggsave("annual_drought.eps", ann_drought, device = "eps", dpi = 72)
+
+ann_drought_hist <- ggplot(a,aes(x=ann.min.flow)) +
+     geom_histogram(breaks = (c(0:10)), color = "black", fill = "gray", na.rm = TRUE) +
+     xlab(TeX('Annual Minimum Discharge $(m^3/s)$')) +
+     ylab("Years") +
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(legend.position="right") + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 12))
+ggsave("annual_drought_hist.eps", ann_drought_hist, device = "eps", dpi = 72)
+
+## Monthly averages
+m <- q %>%
+     mutate(mon=month(dt)) %>%
+     group_by(mon) %>%
+     summarize(mon.mean.flow=mean(flow_m3s,na.rm=TRUE),mon.sd.flow=stdev(flow_m3s),mon.med.flow=median(flow_m3s,na.rm=TRUE)) %>%
+     na_if(Inf) %>%
+     na_if(-Inf) %>%
+     na_if(NaN)
+m$label <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+m$label <- factor(m$label, levels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))
+
+mon_mean <- ggplot(m) +
+     geom_col(aes(x=label,y=mon.mean.flow)) +
+     ylim(c(0,200)) +
+     xlab("Month") + 
+     ylab(TeX('Monthly Mean Discharge $(m^3/s)$')) + 
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(legend.position="right") + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 12))
+ggsave("mon_mean.eps", mon_mean, device = "eps", dpi = 72)
+
+mon_median <- ggplot(m) +
+     geom_col(aes(x=label,y=mon.med.flow)) +
+     ylim(c(0,100)) +
+     xlab("Month") + 
+     ylab(TeX('Monthly Median Discharge $(m^3/s)$')) + 
+     theme(panel.background = element_rect(fill = "white", colour = "black")) + 
+     theme(legend.position="right") + 
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 12))
+ggsave("mon_med.eps", mon_median, device = "eps", dpi = 72)
+
+
+
+
+
+
